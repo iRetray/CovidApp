8a350de688934c1bc00fe016e2c0c626d82ef466
Julian Camilo <julian20141016@gmail.com>
2020-08-11 20:46:03 -0500

    Primary structure

---
+++ src/pages/Countries.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Countries extends React.Component {
+    render() {
+        return(
+            <div>
+                Pagina del Countries
+            </div>
+        )
+    }
+}
