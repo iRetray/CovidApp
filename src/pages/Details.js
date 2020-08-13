c9c9d1f18f19d5232b66826830d85e47606a8945
Julian Camilo <julian20141016@gmail.com>
2020-08-13 09:35:10 -0500

    Add method for get data of route/details/id

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import {Button,Layout} from 'antd';
+import { Button, Layout } from 'antd';
 //import './App.css';
-const{Header}=Layout;
+const { Header } = Layout;
 export default class Details extends React.Component {
     render() {
         var url = document.URL;
-        alert(url);
-        return(
+        // alert(url);
+        return (
             <div>
                 <layout>
                     <Header className="header">
-                        <h1 className="title" id="desc">Pagina  de detalles de:</h1> 
+                        <h1 className="title" id="desc">Pagina  de detalles de:</h1>
+                        {/* Puedes obtener el id que te pasan mediante el parametro de la siguiente forma */}
+                        <h2>Esta pagina muestra los datos del pais con codigo: {this.props.match.params.id}</h2>
                     </Header>
                 </layout>
             </div>
