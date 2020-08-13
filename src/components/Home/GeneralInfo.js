d97a68a1423f0f3665d1bc5d2d65db5b621a5dbf
Julian Camilo <julian20141016@gmail.com>
2020-08-13 14:52:48 -0500

    Version V1.0 of homePage

--- src/components/Home/GeneralInfo.js
+++ src/components/Home/GeneralInfo.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Card, Statistic, Typography } from 'antd'
-import { ArrowUpOutlined, NotificationOutlined } from '@ant-design/icons'
+import { NotificationOutlined, EyeInvisibleOutlined, HeartOutlined } from '@ant-design/icons'
 import Axios from 'axios'
 import Moment from 'moment'
-import "moment/locale/es";
-import CardsInfo from './CardsInfo'
+import "moment/locale/es"
 
 const { Text } = Typography;
 
@@ -19,64 +18,99 @@ export default class GeneralInfo extends React.Component {
         this.getDataGlobal = this.getDataGlobal.bind(this)
     }
 
-    componentDidMount() {
-        this.getDataGlobal()
-    }
-
     async getDataGlobal() {
-        console.log("iniciando peticion")
         const responseData = await Axios.get("https://api.covid19api.com/summary")
             .then(function (response) {
-                console.log("solicitud exitosa")
-                console.log(response);
-                this.setState({
-                    globalData: responseData.data,
-                    generalData: responseData.data.Global
-                })
-                console.log("Estado", this.state.globalData)
-                console.log(this.state.generalData)
-                console.log(this.generalData)
+                return response
             })
             .catch(function (error) {
                 console.log(error);
             })
+        this.setState({
+            globalData: responseData.data,
+            generalData: responseData.data.Global
+        })
+    }
+
+    componentDidMount() {
+        this.getDataGlobal()
     }
 
     render() {
         return (
-            <div>
-                <div className="row container" style={{ justifyContent: 'center' }}>
-                    <div className="col-4-md" style={{ padding: '15px' }}>
-                        <CardsInfo data={this.state.generalData} />
+            <div className="container">
+                <div className="row">
+                    <div className="col-md" style={{ padding: '15px'}}>
                         <Card>
                             <Statistic
-                                title="Nuevos casos confirmados"
-                                value={this.state.globalData.NewConfirmed}
-                                valueStyle={{ color: '#3f8600' }}
+                                title="Nuevos confirmados"
+                                value={this.state.generalData.NewConfirmed}
+                                valueStyle={{ color: '#52c41a' }}
                                 prefix={<NotificationOutlined />}
                                 suffix="personas"
                             />
                         </Card>
-                        {console.log("Hay ")}
-                        {this.state.globalData.NewConfirmed}
                     </div>
-                    <div className="col-4-md" style={{ padding: '15px' }}>
+                    <div className="col-md" style={{ padding: '15px' }}>
                         <Card>
                             <Statistic
-                                title="Recuperados"
-                                value={15.16}
-                                precision={2}
-                                valueStyle={{ color: '#3f8600' }}
-                                prefix={<ArrowUpOutlined />}
-                                suffix="%"
+                                title="Nuevos fallecidos"
+                                value={this.state.generalData.NewDeaths}
+                                valueStyle={{ color: '#fa541c' }}
+                                prefix={<EyeInvisibleOutlined />}
+                                suffix="personas"
                             />
                         </Card>
                     </div>
-
+                    <div className="col-md" style={{ padding: '15px' }}>
+                        <Card>
+                            <Statistic
+                                title="Nuevos recuperados"
+                                value={this.state.generalData.NewRecovered}
+                                valueStyle={{ color: '#2f54eb' }}
+                                prefix={<HeartOutlined />}
+                                suffix="personas"
+                            />
+                        </Card>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md" style={{ padding: '15px' }}>
+                        <Card>
+                            <Statistic
+                                title="Total confirmados"
+                                value={this.state.generalData.TotalConfirmed}
+                                valueStyle={{ color: '#237804' }}
+                                prefix={<NotificationOutlined />}
+                                suffix="personas"
+                            />
+                        </Card>
+                    </div>
+                    <div className="col-md" style={{ padding: '15px' }}>
+                        <Card>
+                            <Statistic
+                                title="Total fallecidos"
+                                value={this.state.generalData.TotalDeaths}
+                                valueStyle={{ color: '#ad2102' }}
+                                prefix={<EyeInvisibleOutlined />}
+                                suffix="personas"
+                            />
+                        </Card>
+                    </div>
+                    <div className="col-md" style={{ padding: '15px' }}>
+                        <Card>
+                            <Statistic
+                                title="Total recuperados"
+                                value={this.state.generalData.TotalRecovered}
+                                valueStyle={{ color: '#10239e' }}
+                                prefix={<HeartOutlined />}
+                                suffix="personas"
+                            />
+                        </Card>
+                    </div>
                 </div>
                 <div style={{ padding: '10px', textAlign: 'right ' }}>
                     <Text type="secondary">Información actualizada a: </Text><Text strong>
-                        {Moment.locale("es")}
                         {Moment(this.state.globalData.Date).format("MMMM D YYYY, h:mm a")}
                     </Text>
                 </div>
