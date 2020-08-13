53b38a5d05fab1e7c143b3fdfb37145dcd9733aa
JhonnyBR <JhonnyBR@users.noreply.github.com>
2020-08-13 08:59:13 -0500

    Juli anda de curioso queriendo ver lo poco que se hace

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import {Button,Layout} from 'antd';
+//import './App.css';
+const{Header}=Layout;
 export default class Details extends React.Component {
     render() {
+        var url = document.URL;
+        alert(url);
         return(
             <div>
-                Pagina del Details
+                <layout>
+                    <Header className="header">
+                        <h1 className="title" id="desc">Pagina de detalles de:</h1> 
+                    </Header>
+                </layout>
             </div>
         )
     }
