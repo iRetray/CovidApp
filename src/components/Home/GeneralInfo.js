6166b63b29631d42ebf306d3d1978f599cd1a764
Julian Camilo <julian20141016@gmail.com>
2020-08-13 13:39:20 -0500

    Add MomentJS and created banner for general cases

--- src/components/Home/GeneralInfo.js
+++ src/components/Home/GeneralInfo.js
@@ -1,50 +1,84 @@
 import React from 'react'
-import { Space, Card, Statistic } from 'antd'
-import { ArrowUpOutlined } from '@ant-design/icons'
+import { Card, Statistic, Typography } from 'antd'
+import { ArrowUpOutlined, NotificationOutlined } from '@ant-design/icons'
 import Axios from 'axios'
+import Moment from 'moment'
+import "moment/locale/es";
+import CardsInfo from './CardsInfo'
+
+const { Text } = Typography;
 
 export default class GeneralInfo extends React.Component {
 
-    constructor (props) {
+    constructor(props) {
         super(props)
         this.state = {
-            globalData: []
+            globalData: {},
+            generalData: {}
         }
         this.getDataGlobal = this.getDataGlobal.bind(this)
     }
 
     componentDidMount() {
-        this.getDataGlobal()        
+        this.getDataGlobal()
     }
 
     async getDataGlobal() {
+        console.log("iniciando peticion")
         const responseData = await Axios.get("https://api.covid19api.com/summary")
+            .then(function (response) {
+                console.log("solicitud exitosa")
+                console.log(response);
+            })
         this.setState({
-            globalData: responseData.data
+            globalData: responseData.data,
+            generalData: responseData.data.Global
         })
-        console.log(this.state.globalData)
+        console.log("Estado", this.state.globalData)
+        console.log(this.state.generalData)
+        console.log(this.generalData)
     }
 
     render() {
         return (
             <div>
-                <Space>
-                    <Card style={{ width: 300 }} hoverable cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}>
-                        <p>Card content</p>
-                        <p>Card content</p>
-                        <p>Card content</p>
-                    </Card>
-                    <Card>
-                        <Statistic
-                            title="Active"
-                            value={11.28}
-                            precision={2}
-                            valueStyle={{ color: '#3f8600' }}
-                            prefix={<ArrowUpOutlined />}
-                            suffix="%"
-                        />
-                    </Card>
-                </Space>
+                <div className="row container" style={{ justifyContent: 'center' }}>
+                    <div className="col-4-md" style={{ padding: '15px' }}>
+                        <CardsInfo data={this.state.generalData} />
+                        <Card>
+                            <Statistic
+                                title="Nuevos casos confirmados"
+                                value={this.state.globalData.NewConfirmed}
+                                valueStyle={{ color: '#3f8600' }}
+                                prefix={<NotificationOutlined />}
+                                suffix="personas"
+                            />
+                        </Card>
+                        {console.log("Hay ")}
+                        {this.state.globalData.NewConfirmed}
+                    </div>
+                    <div className="col-4-md" style={{ padding: '15px' }}>
+                        <Card>
+                            <Statistic
+                                title="Recuperados"
+                                value={15.16}
+                                precision={2}
+                                valueStyle={{ color: '#3f8600' }}
+                                prefix={<ArrowUpOutlined />}
+                                suffix="%"
+                            />
+                        </Card>
+                    </div>
+
+                </div>
+                <div style={{ padding: '10px', textAlign: 'right ' }}>
+                    <Text type="secondary">Información actualizada a: </Text><Text strong>
+                        {
+                            Moment.locale("es"),
+                            Moment(this.state.globalData.Date).format("MMMM D YYYY, h:mm a")
+                        }
+                    </Text>
+                </div>
             </div>
         )
     }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Row, Col } from 'antd'
+import { Row, Col, Typography, Divider } from 'antd'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'antd/dist/antd.css'
 import GeneralInfo from '../components/Home/GeneralInfo'
 import Introduction from '../components/Home/Introduction'
-
+const { Title } = Typography;
 
 export default class Home extends React.Component {
     render() {
@@ -18,6 +18,7 @@ export default class Home extends React.Component {
                     </Col>
                 </Row>
                 <div className="container-fluid" style={{ background: '#F0F2F5', padding: '20px' }}>
+                    <Divider><Title level={4}>Estadísticas globales</Title></Divider>
                     <GeneralInfo />                    
                 </div>
             </div>
