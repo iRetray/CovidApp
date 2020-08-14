36784f414d7cd13429a8bbc68d1dbc52a137bb44
Julian Camilo <julian20141016@gmail.com>
2020-08-14 09:27:15 -0500

    Created state of aplication, add loading in case of petition failure

--- src/components/Home/GeneralInfo.js
+++ src/components/Home/GeneralInfo.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Card, Statistic, Typography } from 'antd'
+import { Card, Statistic, Typography, Divider } from 'antd'
 import { NotificationOutlined, EyeInvisibleOutlined, HeartOutlined } from '@ant-design/icons'
 import Axios from 'axios'
 import Moment from 'moment'
+import LoadingSkeleton from './LoadingSkeleton'
 import "moment/locale/es"
 
 const { Text } = Typography;
@@ -26,10 +27,12 @@ export default class GeneralInfo extends React.Component {
             .catch(function (error) {
                 console.log(error);
             })
-        this.setState({
-            globalData: responseData.data,
-            generalData: responseData.data.Global
-        })
+        if (responseData) {
+            this.setState({
+                globalData: responseData.data,
+                generalData: responseData.data.Global
+            })
+        }
     }
 
     componentDidMount() {
@@ -38,74 +41,100 @@ export default class GeneralInfo extends React.Component {
 
     render() {
         return (
-            <div className="container">
+            <div className="container">                
                 <div className="row">
-                    <div className="col-md" style={{ padding: '15px'}}>
+                    <Divider orientation="left"><Text type="secondary">Nuevos informes</Text></Divider>
+                    <div className="col-md" style={{ padding: '15px' }}>
                         <Card>
-                            <Statistic
-                                title="Nuevos confirmados"
-                                value={this.state.generalData.NewConfirmed}
-                                valueStyle={{ color: '#52c41a' }}
-                                prefix={<NotificationOutlined />}
-                                suffix="personas"
-                            />
+                            {
+                                this.state.generalData.NewConfirmed
+                                    ? <Statistic
+                                        title="Nuevos confirmados"
+                                        value={this.state.generalData.NewConfirmed}
+                                        valueStyle={{ color: '#52c41a' }}
+                                        prefix={<NotificationOutlined />}
+                                        suffix="personas"
+                                    />
+                                    : <div> <LoadingSkeleton /> </div>
+                            }
                         </Card>
                     </div>
                     <div className="col-md" style={{ padding: '15px' }}>
                         <Card>
-                            <Statistic
-                                title="Nuevos fallecidos"
-                                value={this.state.generalData.NewDeaths}
-                                valueStyle={{ color: '#fa541c' }}
-                                prefix={<EyeInvisibleOutlined />}
-                                suffix="personas"
-                            />
+                            {
+                                this.state.generalData.NewConfirmed
+                                    ? <Statistic
+                                        title="Nuevos fallecidos"
+                                        value={this.state.generalData.NewDeaths}
+                                        valueStyle={{ color: '#fa541c' }}
+                                        prefix={<EyeInvisibleOutlined />}
+                                        suffix="personas"
+                                    />
+                                    : <div> <LoadingSkeleton /> </div>
+                            }
                         </Card>
                     </div>
                     <div className="col-md" style={{ padding: '15px' }}>
                         <Card>
-                            <Statistic
-                                title="Nuevos recuperados"
-                                value={this.state.generalData.NewRecovered}
-                                valueStyle={{ color: '#2f54eb' }}
-                                prefix={<HeartOutlined />}
-                                suffix="personas"
-                            />
+                            {
+                                this.state.generalData.NewConfirmed
+                                    ? <Statistic
+                                        title="Nuevos recuperados"
+                                        value={this.state.generalData.NewRecovered}
+                                        valueStyle={{ color: '#2f54eb' }}
+                                        prefix={<HeartOutlined />}
+                                        suffix="personas"
+                                    />
+                                    : <div> <LoadingSkeleton /> </div>
+                            }
                         </Card>
                     </div>
                 </div>
                 <div className="row">
+                    <Divider orientation="left"><Text type="secondary">Informes totales</Text></Divider>
                     <div className="col-md" style={{ padding: '15px' }}>
                         <Card>
-                            <Statistic
-                                title="Total confirmados"
-                                value={this.state.generalData.TotalConfirmed}
-                                valueStyle={{ color: '#237804' }}
-                                prefix={<NotificationOutlined />}
-                                suffix="personas"
-                            />
+                            {
+                                this.state.generalData.NewConfirmed
+                                    ? <Statistic
+                                        title="Total confirmados"
+                                        value={this.state.generalData.TotalConfirmed}
+                                        valueStyle={{ color: '#237804' }}
+                                        prefix={<NotificationOutlined />}
+                                        suffix="personas"
+                                    />
+                                    : <div> <LoadingSkeleton /> </div>
+                            }
                         </Card>
                     </div>
                     <div className="col-md" style={{ padding: '15px' }}>
                         <Card>
-                            <Statistic
-                                title="Total fallecidos"
-                                value={this.state.generalData.TotalDeaths}
-                                valueStyle={{ color: '#ad2102' }}
-                                prefix={<EyeInvisibleOutlined />}
-                                suffix="personas"
-                            />
+                            {
+                                this.state.generalData.NewConfirmed
+                                    ? <Statistic
+                                        title="Total fallecidos"
+                                        value={this.state.generalData.TotalDeaths}
+                                        valueStyle={{ color: '#ad2102' }}
+                                        prefix={<EyeInvisibleOutlined />}
+                                        suffix="personas"
+                                    />
+                                    : <div> <LoadingSkeleton /> </div>
+                            }
                         </Card>
                     </div>
                     <div className="col-md" style={{ padding: '15px' }}>
                         <Card>
-                            <Statistic
-                                title="Total recuperados"
-                                value={this.state.generalData.TotalRecovered}
-                                valueStyle={{ color: '#10239e' }}
-                                prefix={<HeartOutlined />}
-                                suffix="personas"
-                            />
+                            {
+                                this.state.generalData.NewConfirmed
+                                    ? <Statistic
+                                        title="Total recuperados"
+                                        value={this.state.generalData.TotalRecovered}
+                                        valueStyle={{ color: '#10239e' }}
+                                        prefix={<HeartOutlined />}
+                                        suffix="personas"
+                                    />
+                                    : <div> <LoadingSkeleton /> </div>
+                            }
                         </Card>
                     </div>
                 </div>
