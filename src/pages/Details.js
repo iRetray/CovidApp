e97e3ff438b75a16f3282d3b5618a9f1fdc8beb9
Julian Camilo <julian20141016@gmail.com>
2020-08-15 13:25:18 -0500

    Formated content

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,45 +1,47 @@
 import React from 'react'
-import { Row} from 'antd'
+import { Row } from 'antd'
 import Axios from 'axios'
 import 'antd/dist/antd.css';
+
 export default class Details extends React.Component {
-    constructor(props){
+
+    constructor(props) {
         super(props)
-        this.state={
+        this.state = {
             globalAPI: {},
             generalDataAPI: {}
         }
-        this.getDataAPI= this.getDataAPI.bind(this)
+        this.getDataAPI = this.getDataAPI.bind(this)
     }
-    async getDataAPI(){
-        const responseDataAPI=await Axios.get("https://api.covid19api.com/summary")
-        .then(function (response) {
-            return response
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-    if (responseDataAPI) {
-        this.setState({
-            globalAPI: responseDataAPI.data,
-            generalDataAPI: responseDataAPI.data.Global
-        })
-    }
-}
 
-componentDidMount() {
-    this.getDataAPI()
+    async getDataAPI() {
+        const responseDataAPI = await Axios.get("https://api.covid19api.com/summary")
+            .then(function (response) {
+                return response
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        if (responseDataAPI) {
+            this.setState({
+                globalAPI: responseDataAPI.data,
+                generalDataAPI: responseDataAPI.data.Global
+            })
+        }
     }
-    render(){
-        return(
-            
+
+    componentDidMount() {
+        this.getDataAPI()
+    }
+
+    render() {
+        return (
             <div>
-            <Row justify="center">
-            <h2>Esta pagina muestra los datos del pais: {this.props.match.params.id}</h2>
-            </Row>
-            <h2>{ this.state.generalDataAPI.TotalRecovered}</h2>
+                <Row justify="center">
+                    <h2>Esta pagina muestra los datos del pais: {this.props.match.params.id}</h2>
+                </Row>
+                <h2>{this.state.generalDataAPI.TotalRecovered}</h2>
             </div>
-            
         )
     }
 }
