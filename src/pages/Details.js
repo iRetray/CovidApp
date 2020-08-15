1f20a9da4aa7147517cb57b78b179e5343d229a1
JhonnyBR <JhonnyBR@users.noreply.github.com>
2020-08-14 21:03:34 -0500

    connection api with details

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,20 +1,61 @@
 import React from 'react'
-import { Layout, Button } from 'antd'
-//import './App.css';
-const { Header } = Layout
+import { Row} from 'antd'
+import Axios from 'axios'
+import 'antd/dist/antd.css';
 export default class Details extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+            globalAPI: {},
+            generalDataAPI: {}
+        }
+        this.getDataAPI= this.getDataAPI.bind(this)
+    }
+    async getDataAPI(){
+        const responseDataAPI=await Axios.get("https://api.covid19api.com/summary")
+        .then(function (response) {
+            return response
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    if (responseDataAPI) {
+        this.setState({
+            globalAPI: responseDataAPI.data,
+            generalDataAPI: responseDataAPI.data.Global
+        })
+    }
+}
+
+componentDidMount() {
+    this.getDataAPI()
+    }
     render() {
-        // alert(url);
         return (
             <div>
-                <Layout>
-                    <Header className="header">
-                        {/* Puedes obtener el id que te pasan mediante el parametro de la siguiente forma */}
-                        <h2>Esta pagina muestra los datos del pais: {this.props.match.params.id}</h2>
-                        <Button type="primary">Pruebas</Button>
-                    </Header>
-                </Layout>
+                <Row justify="center">
+                <h2>Esta pagina muestra los datos del pais: {this.props.match.params.id}</h2>
+                </Row>
             </div>
+            <div className="col-md" style={{ padding: '15px' }}>
+            <Badge.Ribbon text="Nuevos fallecidos" color="#f5222d" placement="start" >
+                <Card>
+                    {
+                        this.state.generalData.NewConfirmed
+                            ? <center>
+                                <Statistic
+                                value={this.state.generalData.NewDeaths}
+                                valueStyle={{ color: '#f5222d' }}
+                                prefix={<EyeInvisibleOutlined />}
+                                suffix="personas"
+                                style={{ marginTop: '15px' }}
+                            /></center>
+                            : <div> <LoadingSkeleton /> </div>
+                    }
+                </Card>
+            </Badge.Ribbon>
+        </div>
         )
+        
     }
 }
