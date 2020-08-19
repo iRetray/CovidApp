ce939a269a2d50619aa7bf27ad1de8cd97326d12
Julian Camilo <julian20141016@gmail.com>
2020-08-19 10:41:28 -0500

    Fixed NPM package

--- src/pages/Countries.js
+++ src/pages/Countries.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Alert } from 'reactstrap'
 
 export default class Countries extends React.Component {
     render() {
         return(
             <div>
                 Pagina del Countries
+                <Alert>asdasd</Alert>
             </div>
         )
     }
